6609e33e096d45aee41ec552160876eb2ca47fd2
Douglas Volcato <douglasvolcato@gmail.com>
2022-08-17 14:40:39 -0300

    Added controllers

---
+++ src/users/users.route.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const userController = require("./users.controller");
+
+router.get("/", userController.findAllUserController)
+router.post("/", userController.createUserController)
+
+module.exports = router
