d4859cfe5f1199860ece72e869ab734e59eff187
Douglas Volcato <douglasvolcato@gmail.com>
2022-08-18 12:51:21 -0300

    added login controller and service

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,7 +1,22 @@
 const authService = require("./auth.service");
+const bcrypt = require("bcryptjs");
 
 const loginController = async (req, res) => {
-  res.send({ message: "Logged in" });
+  const { email, password } = req.body;
+
+  const user = await authService.loginService(email);
+
+  if (!user) {
+    res.status(400).send({ message: "User not found" });
+  }
+
+  const verifyPassword = await bcrypt.compare(password, user.password);
+
+  if (verifyPassword === false) {
+    return res.status(400).send({ message: "Wrong password" });
+  }
+
+  res.send(user);
 };
 
 module.exports = { loginController };
