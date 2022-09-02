ad89ad80e9b29f3087cd7bfee39e11bf33d44c84
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-02 16:10:03 -0300

    heroku test4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDatabase = require("./src/database/database");
-// const userRoute = require("./src/users/users.route");
+const userRoute = require("./src/users/users.route");
 const authRoute = require("./src/auth/auth.route");
 const tweetsRoute = require("./src/tweets/tweets.route");
 
@@ -13,7 +13,7 @@ connectDatabase();
 app.use(cors());
 app.use(express.json());
 
-// app.use("/users", userRoute);
+app.use("/users", userRoute);
 app.use("/auth", authRoute);
 app.use("/tweets", tweetsRoute);
 
